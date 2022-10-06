717e64c2c4ca4dac64bc5f4603c73ac46c435fac
MohammadALShboul99 <110593677+MohammadALShboul99@users.noreply.github.com>
2022-10-06 17:09:28 +0300

    update Welcome page

--- lib/Welcom.dart
+++ lib/Welcom.dart
@@ -13,80 +13,82 @@ class _WelcomeState extends State<Welcome> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  color: blue,
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(80),
-                  )),
-              child: Image(image: AssetImage("images/welcome.png")),
-            ),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: blue,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
               child: Container(
                 width: double.infinity,
                 decoration: const BoxDecoration(
-                    color: Colors.white,
+                    color: blue,
                     borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(100),
+                      bottomRight: Radius.circular(80),
                     )),
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Text(
-                        "Learning English ",
+                child: Image(image: AssetImage("images/welcome.png")),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: blue,
+                child: Container(
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(100),
+                      )),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          "Learning English ",
+                          style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "learn English in any \n place with us",
                         style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold),
+                            fontSize: 20, fontWeight: FontWeight.w500),
                       ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      "learn English in any \n place with us",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                    ),
-                    Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          MaterialButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, "Login_page");
-                            },
-                            height: 50,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20)),
-                            color: pink,
-                            child: Text(
-                              "Get started",
-                              style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
+                      Spacer(),
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            MaterialButton(
+                              onPressed: () {
+                                Navigator.pushNamed(context, "Login_page");
+                              },
+                              height: 50,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              color: pink,
+                              child: Text(
+                                "Get started",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ],
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
